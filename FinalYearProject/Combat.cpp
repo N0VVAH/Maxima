a3065e7e48d8491fa505a8cca98b493c2caa94e8
NOVVAH <h_az_2000@hotmail.co.uk>
2023-02-05 14:49:56 +0000

    Added Fill Colour Function for Squares Created buttons to move through move options

--- FinalYearProject/Combat.cpp
+++ FinalYearProject/Combat.cpp
@@ -32,11 +32,11 @@ combat::combat(scene* prev)
 
 	//menu
 	rightMove = Square(sf::Color::Black, 50, 25);
-	rightMove.setPos(1325, 705);
+	rightMove.setPos(1245, 705);
 	//render.push_back(&rightMove);
 
 	leftMove = Square(sf::Color::Black, 50, 25);
-	leftMove.setPos(1255, 705);
+	leftMove.setPos(1175, 705);
 	//render.push_back(&leftMove);
 
 
@@ -144,24 +144,45 @@ void combat::changeButtons(char butt)
 	}
 
 	int moveCount;
-
+	int start;
 	switch (butt)
 	{
 	case 'F':
 		moveCount = Global::Player->moves.size();
-		for (size_t i = 0; i < moveCount; i++)
+		start = 0;
+		if (moveCount > 10) { moveCount = 10; }
+
+		for (size_t i = start; i < moveCount; i++)
 		{
 			if (i % 2 == 0)
 			{
 				moves.push_back(new Square(sf::Color::Black, 100, 35));
 				moves[i]->setPos(1100, 500 + ((i / 2) * 50));
-				render.push_back(moves[i]);
+				UI.push_back(moves[i]);
 			}
 			else
 			{
 				moves.push_back(new Square(sf::Color::Black, 100, 35));
 				moves[i]->setPos(1220, 500 + ((i / 2) * 50));
-				render.push_back(moves[i]);
+				UI.push_back(moves[i]);
+			}
+			UI.push_back(&leftMove);
+			UI.push_back(&rightMove);
+			if (moveCount == 10 && (start + 10) < Global::Player->moves.size())
+			{
+				rightMove.setFillColour(sf::Color::Black);
+			}
+			else
+			{
+				rightMove.setFillColour(sf::Color::White);
+			}
+			if (start != 0)
+			{
+				leftMove.setFillColour(sf::Color::Black);
+			}
+			else
+			{
+				leftMove.setFillColour(sf::Color::White);
 			}
 
 		}
